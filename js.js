2e8972370a780c8d2b25fafa5c8e538f430e83db
js <js82965923@live.com>
2017-10-26 17:39:17 +0800

    swal2，新荧幕结构，同步展示与数据，几个常用样式，箭头按键操作
    还有多重样式、剩余属性、多文件同样式合并、切换全屏比利、等问题没有解决

--- js.js
+++ js.js
@@ -23,8 +23,9 @@ window.addEventListener('load', function () {
 				var reader = new FileReader();
 				reader.addEventListener("load", function () {
 				//ass开头[Script Info]区
+
 					var obj = {};
-					obj.name = file.name;
+					obj.name = file.name.replace(/\.[^.]*$/, '');
 					obj.PlayResX = parseInt(matchAgain(matchAgain(reader.result,/(?=PlayResX).*/),/\d+/));
 					obj.PlayResY = parseInt(matchAgain(matchAgain(reader.result,/(?=PlayResY).*/),/\d+/));
 					obj.content = reader.result;
@@ -56,11 +57,25 @@ window.addEventListener('load', function () {
 						if(x == 'style'){
 							for(y in obj.style[0]){
 								var inputStyle = document.createElement('input');
+								inputStyle.type = 'text';
+
 								inputStyle.value = obj.style[0][y];
+
+								inputStyle.addEventListener('focus',function(){
+									this.focusInput = true;
+								});
+								inputStyle.addEventListener('blur',function(){
+									this.focusInput = false;
+								});
+
+								inputStyle.id = y + '_' + obj.name.replace(/\.[^.]*$/, '') + '_style_0';
+								inputStyle.title = matchStyle(y);
+								inputStyle.name = obj.name.replace(/\.[^.]*$/, '');
 								inputAll.push(inputStyle);
 							}
 							continue;
 						}
+					//文件基本宽高
 						var input = document.createElement('input');
 						input.type = 'text';
 						input.value = obj[x];
@@ -72,80 +87,59 @@ window.addEventListener('load', function () {
 						});
 						input.id = x + '_' + obj.name.replace(/\.[^.]*$/, '');
 						input.title = matchStyle(x);
-						input.name = 'name_' + obj.name.replace(/\.[^.]*$/, '');
+						input.name = obj.name.replace(/\.[^.]*$/, '');
 						inputAll.push(input);
 					}
 					for(i=0;i<inputAll.length;i++){
-						cln.children[0].children[0].appendChild(inputAll[i]);
+						cln.children[0].children[1].children[0].appendChild(inputAll[i]);
 					}
 				//定位修饰字幕元素
-					//(function locateChild(parentObj){
-					//	if(parentObj.className.indexOf(frameConfig.tagName)+1){
-					//		parentObj.innerText = tar.innerText;
-					//		return
-					//	}
-					//	if(parentObj.children.length == 0) return;
-					//	for(i=0;i<parentObj.children.length;i++){
-					//		locateChild(parentObj.children[i])
-					//	}
-					//})(cln)
-					(function locateChild(parentObj){
-						for(i=0;i<parentObj.children.length;i++){
-							if(parentObj.children[i].id.indexOf('sub')+1){
-								parentObj.children[i].id = 'sub_' + file.name.replace(/\.[^.]*$/, '');
-								//console.log(parentObj.children[i]);
-								//parentObj.children[i].style.fontSize = obj.style[0].Fontsize + 'px';
+					;(function locateChild(parentObj){
+						if(parentObj.id.indexOf('sub')+1){
+							parentObj.id = 'sub_' + file.name.replace(/\.[^.]*$/, '');
 							//先娶第一个样式测试
-								for(x in obj.style[0]){
-									//console.log(x);
-									//console.log(parentObj.children[i]);
+							for(x in obj.style[0]){
 								//字幕字号适应元素尺寸
-									if(x.indexOf('Fontsize')+1){
-										parentObj.children[i].style[matchStyle(x)] = ((tpl.clientHeight*obj.style[0][x])/obj.PlayResY)+'px';
-										//console.log(x);
-										//console.log(obj.style[0][x]);
-										//console.log(obj.PlayResY);
-										//console.log(tpl.clientHeight);
-										continue
-									}
-								//定义描边样式
-									if(x == 'Outline'){
-										(function locateChildA(locateObj){
-											for(j=0;j<locateObj.children.length;j++){
-												if(locateObj.children[j].className.indexOf('stroke')+1){
-													locateObj.children[j].style[matchStyle(x)] = obj.style[0][x] *2 +'px';
-													return
-												}
-												if(locateObj.children.length == 0) return;
-												locateChildA(locateObj.children[j])
-											}
-										})(parentObj.children[i]);
-										//locateChildA(parentObj.children[i],'className','stroke').style[matchStyle(x)] = obj.style[0][x] *2 +'px';
-										continue
-									}
-								//描边颜色
-									if(x == 'OutlineColour'){
-										(function locateChildA(locateObj){
-											for(j=0;j<locateObj.children.length;j++){
-												if(locateObj.children[j].className.indexOf('stroke')+1){
-													var rgb = obj.style[0][x].match(/.{2}/g);
-													locateObj.children[j].style[matchStyle(x)] = '#'+rgb[4]+rgb[3]+rgb[2];
-													locateObj.children[j].style.opacity = ((256-parseInt(rgb[1],16))*(1/256)).toFixed(2);
-													return
-												}
-												if(locateObj.children.length == 0) return;
-												locateChildA(locateObj.children[j])
-											}
-										})(parentObj.children[i]);
-										//locateChildA(parentObj.children[i],'className','stroke').style[matchStyle(x)] = '#'+obj.style[0][x].match(/\w{6}\b/)[0];
-										//console.log(obj.style[0][x].match(/\w{6}\b/)[0]);
-										continue
-									}
-									parentObj.children[i].style[matchStyle(x)] = parseInt(obj.style[0][x])?obj.style[0][x]+'px': obj.style[0][x]
+								if(x.indexOf('Fontsize')+1){
+									parentObj.style[matchStyle(x)] = ((tpl.clientHeight*obj.style[0][x])/obj.PlayResY)+'px';
+									continue
 								}
-								return
+								//定义描边样式
+								if(x == 'Outline'){
+									(function locateChildA(locateObj){
+										if(locateObj.className.indexOf('stroke')+1){
+											locateObj.style[matchStyle(x)] = obj.style[0][x] *2 +'px';
+											return
+										}
+										if(locateObj.children.length == 0) return;
+										for(j=0;j<locateObj.children.length;j++){
+											locateChildA(locateObj.children[j])
+										}
+									})(parentObj);
+									continue
+								}
+								//描边颜色
+								if(x == 'OutlineColour'){
+									(function locateChildA(locateObj){
+										if(locateObj.className.indexOf('stroke')+1){
+											var rgb = obj.style[0][x].match(/.{2}/g);
+											locateObj.style[matchStyle(x)] = '#'+rgb[4]+rgb[3]+rgb[2];
+											locateObj.style.opacity = ((256-parseInt(rgb[1],16))*(1/256)).toFixed(2);
+											return
+										}
+										if(locateObj.children.length == 0) return;
+										for(j=0;j<locateObj.children.length;j++){
+											locateChildA(locateObj.children[j])
+										}
+									})(parentObj);
+									continue
+								}
+								parentObj.style[matchStyle(x)] = parseInt(obj.style[0][x])?obj.style[0][x]+'px': obj.style[0][x]
 							}
-							if(parentObj.children.length == 0) return;
+							return
+						}
+						if(parentObj.children.length == 0) return;
+						for(var i=0;i<parentObj.children.length;i++){
 							locateChild(parentObj.children[i])
 						}
 					})(cln);
@@ -174,12 +168,12 @@ window.addEventListener('load', function () {
 					var s0 = obj.style[0];
 					var pushStyle = function(styleText){
 						var bTemp = styleText.replace(/ /g,'').split(/,/);
-						var str1 = 'Style:';
+						var strTemp = 'Style:';
 						for(j=0;j<bTemp.length;j++){
-							j==0?void(0):str1 +=',';
-							str1 +=s0[bTemp[j]];
+							j==0?void(0):strTemp +=',';
+							strTemp +=s0[bTemp[j]];
 						}
-						return str1
+						return strTemp
 					};
 					var str = pushStyle(' Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding');
 					console.log(str);
@@ -187,7 +181,7 @@ window.addEventListener('load', function () {
 					return temp
 				};
 				console.log('\ufeff' + pushConfig(config[i]));
-				//startDownload('\ufeff' + aaa(config[i]), config[i].name.replace(/\.[^.]*$/, '') + '.ass');
+				startDownload('\ufeff' + pushConfig(config[i]), config[i].name.replace(/\.[^.]*$/, '') + '.ass');
 			}
 	//全屏
 		}else if(target.className.indexOf('full')+1){
@@ -245,10 +239,45 @@ var matchAgain = function (text,regex) {
 	return text.toString().match(reg)||0;
 };
 //
-var pushChange = function(num,name,title){
-	//console.log(title);
-	var temp = document.getElementById(name);
-	temp.children[0].style[title] = num+'px'
+var pushChange = function(num,name,title,id){
+	for(var i=0;i<config.length;i++){
+		if(config[i].name == name) config[i].style[0][id.split("_")[0]] = num
+	}
+	var sub = document.getElementById('name_'+name);
+	var tempPlayResY = document.getElementById('PlayResY_'+id.split("_")[1]);
+	var tempFontsize = document.getElementById('Fontsize_'+id.split("_")[1]+'_style_0');
+	;(function locateChild(obj){
+		if(obj.className.indexOf('stroke')+1){
+			if(id.indexOf('Outline_')+1){
+				obj.style[title] = num *2 +'px';
+				return
+			}
+			if(id.indexOf('OutlineColour_')+1){
+				var rgb = num.match(/.{2}/g);
+				obj.style[title] = '#'+rgb[4]+rgb[3]+rgb[2];
+				obj.style.opacity = ((256-parseInt(rgb[1],16))*(1/256)).toFixed(2);
+				return
+			}
+		}
+		if(obj.id.indexOf('sub_')+1){
+			if(id.indexOf('Fontname_')+1){
+				obj.style[title] = num;
+				return
+			}
+			if(id.indexOf('Fontsize_')+1){
+				obj.style[title] = ((sub.clientHeight*num)/tempPlayResY.value)+'px';
+				return
+			}
+			if(id.indexOf('PlayResY_')+1){
+				obj.style.fontSize = ((sub.clientHeight*tempFontsize.value)/num)+'px';
+				return
+			}
+		}
+		if(obj.children.length == 0) return;
+		for(var i=0;i<obj.children.length;i++){
+			locateChild(obj.children[i]);
+		}
+	})(sub);
 };
 // Handle keyboard controls
 var keysDown = {};
@@ -256,15 +285,32 @@ addEventListener("keydown", function (e) {
 	keysDown[e.keyCode] = true;
 	if(e.target.focusInput){
 		if (38 in keysDown ) { // Player holding up || w
-			e.target.value++;
-			pushChange(e.target.value,e.target.name,e.target.title)
+			parseInt(e.target.value)||e.target.value == 0?++e.target.value:swErr();
+			pushChange(e.target.value,e.target.name,e.target.title,e.target.id)
 		}
 		if (40 in keysDown ) { // Player holding down || s
-			e.target.value--;
-			pushChange(e.target.value,e.target.name,e.target.title)
+			parseInt(e.target.value)||e.target.value == 0?--e.target.value:swErr();
+			pushChange(e.target.value,e.target.name,e.target.title,e.target.id)
+		}
+		if (13 in keysDown ) { //enter
+			//e.target.blur();
+			pushChange(e.target.value,e.target.name,e.target.title,e.target.id)
+		}
+		if (27 in keysDown ) { //escape
+			e.target.blur();
+			pushChange(e.target.value,e.target.name,e.target.title,e.target.id)
 		}
 	}
 }, false);
 addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
+var swErr = function(){
+	swal({
+		title: "目前这玩意还不能上下选择",
+		text: "这个框框一会会自己关闭",
+		timer: 1500,
+		button: "关！",
+		icon: "error"
+	});
+};
